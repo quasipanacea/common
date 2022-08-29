bee8e4269a09e9bb370cae16c254138ee188e4f5
Edwin Kofler <edwin@kofler.dev>
2022-08-29 01:45:54 -0700

    update: Types for new server routes

--- schema.ts
+++ schema.ts
@@ -1,55 +1,151 @@
 import { z } from "zod";
 
+export const documentKinds = z.enum(["KindSingle", "KindCoupled"]);
+export type documentKindsType = z.infer<typeof documentKinds>;
+
 /* ---------------------- document ---------------------- */
 
-// documentRead
-export const documentCreateReq = z.object({
-	type: z.enum(["type-regular", "type-unique"]),
+// create single
+export const documentCreateSingleRequest = z.object({
 	name: z.string().min(1),
 });
-export const documentCreateRes = z.object({});
-export type documentCreateReqType = z.infer<typeof documentCreateReq>;
-export type documentCreateResType = z.infer<typeof documentCreateRes>;
+export type documentCreateSingleRequestType = z.infer<
+	typeof documentCreateSingleRequest
+>;
+export const documentCreateSingleResponse = z.object({
+	success: z.boolean(),
+});
+export type documentCreateSingleResponseType = z.infer<
+	typeof documentCreateSingleResponse
+>;
 
-// documentRead
-export const documentReadReq = z.object({
-	type: z.enum(["type-regular", "type-unique"]),
+// create couple
+export const documentCreateCoupledRequest = z.object({
+	channel: z.string().min(1),
+	id: z.string().min(1),
+});
+export type documentCreateCoupledRequestType = z.infer<
+	typeof documentCreateCoupledRequest
+>;
+export const documentCreateCoupledResponse = z.object({
+	success: z.boolean(),
+});
+export type documentCreateCoupledResponseType = z.infer<
+	typeof documentCreateCoupledResponse
+>;
+
+// read couple
+export const documentReadSingleRequest = z.object({
 	name: z.string().min(1),
 });
-export const documentReadRes = z.object({});
+export type documentReadSingleRequestType = z.infer<
+	typeof documentReadSingleRequest
+>;
+export const documentReadSingleResponse = z.object({
+	content: z.string(),
+});
+export type documentReadSingleResponseType = z.infer<
+	typeof documentReadSingleResponse
+>;
 
-export type documentReadReqType = z.infer<typeof documentReadReq>;
-export type documentReadResType = z.infer<typeof documentReadRes>;
+// read couple
+export const documentReadCoupleRequest = z.object({
+	channel: z.string().min(1),
+	id: z.string().min(1),
+});
+export type documentReadCoupleRequestType = z.infer<
+	typeof documentReadCoupleRequest
+>;
+export const documentReadCoupleResponse = z.object({
+	content: z.string(),
+});
+export type documentReadCoupleResponseType = z.infer<
+	typeof documentReadCoupleResponse
+>;
 
-// documentWrite
-export const documentWriteReq = z.object({
-	type: z.enum(["type-regular", "type-unique"]),
+// write single
+export const documentWriteSingleRequest = z.object({
 	name: z.string().min(1),
 	content: z.string(),
 });
-export const documentWriteRes = z.object({});
-export type documentWriteReqType = z.infer<typeof documentWriteReq>;
-export type documentWriteResType = z.infer<typeof documentWriteRes>;
+export type documentWriteSingleRequestType = z.infer<
+	typeof documentWriteSingleRequest
+>;
 
-// documentDelete
-export const documentDeleteReq = z.object({
-	type: z.enum(["type-regular", "type-unique"]),
+export const documentWriteSingleResponse = z.object({});
+export type documentWriteSingleResponseType = z.infer<
+	typeof documentWriteSingleResponse
+>;
+
+// write couple
+export const documentWriteCoupleRequest = z.object({
+	channel: z.string().min(1),
+	id: z.string(),
+	content: z.string(),
+});
+export type documentWriteCoupleRequestType = z.infer<
+	typeof documentWriteCoupleRequest
+>;
+export const documentWriteCoupleResponse = z.object({});
+export type documentWriteCoupleResponseType = z.infer<
+	typeof documentWriteCoupleResponse
+>;
+
+// delete single
+export const documentDeleteSingleRequest = z.object({
 	name: z.string().min(1),
 });
-export const documentDeleteRes = z.object({});
-export type documentDeleteReqType = z.infer<typeof documentDeleteReq>;
-export type documentDeleteResType = z.infer<typeof documentDeleteRes>;
+export type documentDeleteSingleRequestType = z.infer<
+	typeof documentDeleteSingleRequest
+>;
+export const documentDeleteSingleResponse = z.object({});
+export type documentDeleteSingleResponseType = z.infer<
+	typeof documentDeleteSingleResponse
+>;
 
-// documentList
-export const documentListReq = z.object({
-	type: z.enum(["type-regular", "type-unique"]),
+// delete couple
+export const documentDeleteCoupleRequest = z.object({
+	channel: z.string().min(1),
+	id: z.string().min(1),
 });
-export const documentListRes = z.object({
+export type documentDeleteCoupleRequestType = z.infer<
+	typeof documentDeleteCoupleRequest
+>;
+export const documentDeleteCoupleResponse = z.object({});
+export type documentDeleteCoupleResponseType = z.infer<
+	typeof documentDeleteCoupleResponse
+>;
+
+// list single
+export const documentListSingleRequest = z.object({});
+export type documentListSingleRequestType = z.infer<
+	typeof documentListSingleRequest
+>;
+export const documentListSingleResponse = z.object({
 	documents: z.array(
 		z.object({
 			name: z.string().min(1),
 		})
 	),
 });
-export type documentListReqType = z.infer<typeof documentListReq>;
-export type documentListResType = z.infer<typeof documentListRes>;
+export type documentListSingleResponseType = z.infer<
+	typeof documentListSingleResponse
+>;
+
+// list couple
+export const documentListCoupleRequest = z.object({
+	channel: z.string().min(1),
+});
+export type documentListCoupleRequestType = z.infer<
+	typeof documentListCoupleRequest
+>;
+export const documentListCoupleResponse = z.object({
+	documents: z.array(
+		z.object({
+			name: z.string().min(1),
+		})
+	),
+});
+export type documentListCoupleResponseType = z.infer<
+	typeof documentListCoupleResponse
+>;
