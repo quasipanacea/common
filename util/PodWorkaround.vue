a386c40591872f6b2d66465a64b8c780b83b6e90
Edwin Kofler <edwin@kofler.dev>
2022-12-31 14:17:25 -0800

    add pod workaround

---
+++ util/PodWorkaround.vue
@@ -0,0 +1,21 @@
+<template>
+	<PodMarkdown v-if="type === 'markdown'" />
+	<PodMarkdown v-else-if="type === 'plaintext'" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import PodMarkdown from '../../common/plugins/Core/PodMarkdown/client-web/PodView.vue'
+import PodPlaintext from '../../common/plugins/Core/PodPlaintext/client-web/PodView.vue'
+
+export default defineComponent({
+	props: {
+		type: String,
+	},
+	components: {
+		PodMarkdown,
+		PodPlaintext,
+	},
+})
+</script>
